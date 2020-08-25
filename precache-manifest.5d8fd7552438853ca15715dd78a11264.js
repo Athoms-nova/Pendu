2d0a760bc6d27506b2d9996f9ffd3af671679b98
unknown <lova.raheri@gmail.com>
2020-08-25 21:44:26 +0200

    Updates

---
+++ precache-manifest.5d8fd7552438853ca15715dd78a11264.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b18be661735f101107dadf319ea1a1d",
+    "url": "/Pendu/index.html"
+  },
+  {
+    "revision": "bc10b3e798ae2453ee85",
+    "url": "/Pendu/static/css/main.fc99456f.chunk.css"
+  },
+  {
+    "revision": "edaddb7b20cac1398cab",
+    "url": "/Pendu/static/js/2.c89e0530.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pendu/static/js/2.c89e0530.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc10b3e798ae2453ee85",
+    "url": "/Pendu/static/js/main.17cc0e77.chunk.js"
+  },
+  {
+    "revision": "12c1b439f3dbb1a63130",
+    "url": "/Pendu/static/js/runtime-main.a8a98f44.js"
+  },
+  {
+    "revision": "031dd56a55a63b1df57fa90baa1615fb",
+    "url": "/Pendu/static/media/frame0.031dd56a.png"
+  },
+  {
+    "revision": "1dc1d24e2e0c26d693c2254cf8c00fc1",
+    "url": "/Pendu/static/media/frame1.1dc1d24e.png"
+  },
+  {
+    "revision": "dd047152a515055fddce7b04b2afcd07",
+    "url": "/Pendu/static/media/frame10.dd047152.png"
+  },
+  {
+    "revision": "efebbb83d5b993c299ac9659e313de58",
+    "url": "/Pendu/static/media/frame2.efebbb83.png"
+  },
+  {
+    "revision": "33736aeb8b6440e636b4492238b55c34",
+    "url": "/Pendu/static/media/frame3.33736aeb.png"
+  },
+  {
+    "revision": "a97f9f6057a8b079e7736d15bd8c8948",
+    "url": "/Pendu/static/media/frame4.a97f9f60.png"
+  },
+  {
+    "revision": "2f18f721527fe90ce6f45f12572f8d84",
+    "url": "/Pendu/static/media/frame5.2f18f721.png"
+  },
+  {
+    "revision": "12cc0641dee3a683624302f9896dcf1c",
+    "url": "/Pendu/static/media/frame6.12cc0641.png"
+  },
+  {
+    "revision": "e106ee5a2b9c4dce16f8b5d38737f145",
+    "url": "/Pendu/static/media/frame7.e106ee5a.png"
+  },
+  {
+    "revision": "642716e668e576299b3e115abb7caeaa",
+    "url": "/Pendu/static/media/frame8.642716e6.png"
+  },
+  {
+    "revision": "080473af0ab8f8e5c5119c51cfae5643",
+    "url": "/Pendu/static/media/frame9.080473af.png"
+  },
+  {
+    "revision": "5e455adad9faa93f4bc58cb418fd4192",
+    "url": "/Pendu/static/media/jeuFondVide.5e455ada.png"
+  },
+  {
+    "revision": "124442b968a41a0d896336bcd0613560",
+    "url": "/Pendu/static/media/jeuGagner.124442b9.png"
+  },
+  {
+    "revision": "8c4d4836d93b089cfdd1051f82c1f2ed",
+    "url": "/Pendu/static/media/jeuOrange.8c4d4836.png"
+  },
+  {
+    "revision": "6cfc31078bdb6e2685d425e1d76c1e9e",
+    "url": "/Pendu/static/media/jeuPerdu.6cfc3107.png"
+  },
+  {
+    "revision": "075d1bf591ae51a95201c44c6e1937e5",
+    "url": "/Pendu/static/media/jeuRouge.075d1bf5.png"
+  },
+  {
+    "revision": "131f178ebc8a933dfd07d2d92869c162",
+    "url": "/Pendu/static/media/jeuVert.131f178e.png"
+  }
+]);
